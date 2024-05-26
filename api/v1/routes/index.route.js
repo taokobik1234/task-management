1d7594de5f8e7e93aa16eba1c72da8865fd6c0ff
taokobik1234 <thienvovinpro123@gmail.com>
2024-05-26 22:17:27 +0700

    Create routes

---
+++ api/v1/routes/index.route.js
@@ -0,0 +1,7 @@
+const taskRoute = require("./task.route");
+
+module.exports = (app) =>{
+    const version = "/api/v1";
+    app.use(version+"/tasks",taskRoute);
+    
+}
--- index.js
+++ index.js
@@ -1,36 +1,14 @@
 const express = require('express');
 const database = require("./config/database");
 require("dotenv").config();
+
+const routesApiVer1 = require("./api/v1/routes/index.route")
 const app = express();
 const port = process.env.PORT;
 
 database.connect();
-
-const Task = require("./models/task.model")
-app.get("/tasks",async (req,res)=>{
-    const tasks = await Task.find({
-        deleted: false
-    });
-
-    res.json(tasks);
-});
-
-app.get("/tasks/detail/:id",async (req,res)=>{
-
-    try {
-        const id = req.params.id;
-
-        const tasks = await Task.findOne({
-            _id: id,
-            deleted: false
-        });
-
-        res.json(tasks);
-    } catch (error) {
-        res.json("Not Find");
-    }
-    
-});
+// route version1
+routesApiVer1(app);
 
 app.listen(port,()=>{
     console.log(`App listening on port ${port}`);
